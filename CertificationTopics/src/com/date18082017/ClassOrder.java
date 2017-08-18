de95017a6b3596b127e316b88678571537b6b3be
Padovese <bro_sk8@hotmail.com>
2017-08-18 17:03:17 -0300

    updating from 18/08/2017

---
+++ CertificationTopics/src/com/date18082017/ClassOrder.java
@@ -0,0 +1,11 @@
+package com.date18082017;
+
+public class ClassOrder {
+	 public static void main(String[] args) {
+	 System.out.print(Order.result + " ");
+	 System.out.print(Order.result + " ");
+	 //new Order();
+	 //new Order();
+	 System.out.print(Order.result + " ");
+	 }
+	 }
---
+++ CertificationTopics/src/com/date18082017/Order.java
@@ -0,0 +1,10 @@
+package com.date18082017;
+
+public class Order {
+	 static String result = "";
+	 { result += "c"; }
+	 static
+	 { result += "u"; }
+	 { result += "r"; }
+	 }
+
