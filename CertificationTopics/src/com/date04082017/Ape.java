a36e8189b2c519561fca8f58ad83cbd9f4b6e357
Padovese <bro_sk8@hotmail.com>
2017-09-04 19:00:50 -0300

    testing somethings

---
+++ CertificationTopics/src/com/date04082017/Ape.java
@@ -0,0 +1,12 @@
+package com.date04082017;
+
+public class Ape extends Monkey{
+	public static int a() {
+		System.out.println("a2");
+		return 2;
+	}
+	
+	public static void main(String[] args) {
+		a();
+	}
+}
---
+++ CertificationTopics/src/com/date04082017/Monkey.java
@@ -0,0 +1,8 @@
+package com.date04082017;
+
+public class Monkey {
+	protected static int a() {
+		System.out.println("a");
+		return 1;
+	}
+}
