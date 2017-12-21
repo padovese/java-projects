e32de1836e23b36feb10d767c0b4f615e785a309
padovese <bhspadoveze@gmail.com>
2017-12-21 20:04:54 -0200

    leafrning mvc

--- SpringMVCTutorial/src/com/controller/HelloWorld.java
+++ SpringMVCTutorial/src/com/controller/HelloWorld.java
@@ -3,15 +3,21 @@ package com.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
- 
+
 @Controller
 public class HelloWorld {
- 
+
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
- 
+
 		String message = "<br><div style='text-align:center;'>"
 				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from HelloWorld.java **********</div><br><br>";
 		return new ModelAndView("welcome", "message", message);
+	}
+
+	public void test() {
+		int a = 0;
+
+		int b = 1;
 	}
 }
