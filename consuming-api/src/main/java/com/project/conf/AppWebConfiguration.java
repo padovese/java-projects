35466bf8ea1b9c10c9da53e134c9cbd9d34893b6
padovese <bhspadoveze@gmail.com>
2018-06-24 23:44:40 -0300

    uploading consuming-api project

---
+++ consuming-api/src/main/java/com/project/conf/AppWebConfiguration.java
@@ -0,0 +1,47 @@
+package com.project.conf;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.project"})
+public class AppWebConfiguration implements WebMvcConfigurer{
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/views/");
+		resolver.setSuffix(".jsp");
+
+		return resolver;
+
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+
+		messageSource.setBasename("/WEB-INF/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setCacheSeconds(1);
+		return messageSource;
+	}
+	
+	   @Override
+	   public Validator getValidator() {
+	      LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+	      validator.setValidationMessageSource(messageSource());
+	      return validator;
+	   }
+}
