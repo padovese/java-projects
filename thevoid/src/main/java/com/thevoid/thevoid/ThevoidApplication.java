306aadf291c705cf316fba9773b373d8cf4a6fdb
padovese <bhspadoveze@gmail.com>
2018-06-28 23:32:15 -0300

    New project

---
+++ thevoid/src/main/java/com/thevoid/thevoid/ThevoidApplication.java
@@ -0,0 +1,12 @@
+package com.thevoid.thevoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThevoidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThevoidApplication.class, args);
+	}
+}
