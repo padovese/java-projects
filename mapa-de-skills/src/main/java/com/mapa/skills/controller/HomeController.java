276a5bbcdce7ad7b2f84bbe51c297681849a7351
padovese <bhspadoveze@gmail.com>
2018-06-29 23:30:24 -0300

    adding new project and fixing the dependency problem

--- mapa-de-skills/src/main/java/com/mapa/skills/controller/HomeController.java
+++ mapa-de-skills/src/main/java/com/mapa/skills/controller/HomeController.java
@@ -1,13 +1,18 @@
-package com.thevoid.controller;
+package com.mapa.skills.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping(value = "/")
+
+	@RequestMapping("/")
 	public String home() {
-		return "index";
+		return "home";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
 	}
 }
--- thevoid/src/main/java/com/thevoid/ThevoidApplication.java
+++ thevoid/src/main/java/com/thevoid/ThevoidApplication.java
@@ -1,9 +1,11 @@
-package com.thevoid.thevoid;
+package com.thevoid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.thevoid")
 public class ThevoidApplication {
 
 	public static void main(String[] args) {
