7f83453b30d3864e8ab57100d4f3836f082a11c1
padovese <bhspadoveze@gmail.com>
2018-09-09 13:09:36 -0300

    using @Rule instead of @RunWith

--- mockito-tests/src/test/java/br/com/gypsydanger/business/TodoBusinessImplMockTest.java
+++ mockito-tests/src/test/java/br/com/gypsydanger/business/TodoBusinessImplMockTest.java
@@ -14,18 +14,24 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import br.com.gypsydanger.data.api.TodoService;
 
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 public class TodoBusinessImplMockTest {
 
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
+	
 	@Mock
 	TodoService todoService;
 	
