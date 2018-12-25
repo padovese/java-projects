22a258f763dd4b7c009694981037fc2095ffb058
padovese <bhspadoveze@gmail.com>
2018-12-25 20:22:17 -0200

    improving the save user method

--- restful-web-services/src/main/java/com/gypsydanger/rest/webservices/restfulwebservices/controller/UserController.java
+++ restful-web-services/src/main/java/com/gypsydanger/rest/webservices/restfulwebservices/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.gypsydanger.rest.webservices.restfulwebservices.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gypsydanger.rest.webservices.restfulwebservices.model.User;
 import com.gypsydanger.rest.webservices.restfulwebservices.service.UserDaoService;
@@ -29,7 +32,12 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
-		userService.save(user);
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
+		User savedUser = userService.save(user);
+		
+		//Necessary to return status 201 (created) and the link to the new created resource. Best practices instead of return only 200.
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 }
