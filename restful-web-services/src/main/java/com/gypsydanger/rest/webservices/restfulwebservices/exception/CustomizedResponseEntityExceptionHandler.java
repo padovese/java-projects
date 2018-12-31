486221e9a3719c00d9d43df0c16b0b5aad067012
padovese <bhspadoveze@gmail.com>
2018-12-31 12:10:04 -0200

    Customnize erro handling treatment

---
+++ restful-web-services/src/main/java/com/gypsydanger/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.gypsydanger.rest.webservices.restfulwebservices.exception;
+
+import java.time.LocalDate;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+}
