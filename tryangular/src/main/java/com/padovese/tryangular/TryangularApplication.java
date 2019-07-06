de3020497f42c120d974811e6c649ffb58df6ae3
padovese <bhspadoveze@gmail.com>
2019-07-06 01:09:41 -0300

    gitgnore

---
+++ tryangular/src/main/java/com/padovese/tryangular/TryangularApplication.java
@@ -0,0 +1,13 @@
+package com.padovese.tryangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryangularApplication.class, args);
+	}
+
+}
